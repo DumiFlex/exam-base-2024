6ba98528a7598bfcc37a4b2421d6c8d8c951c31e
DumiFlex <94956983+DumiFlex@users.noreply.github.com>
2025-02-11 13:18:32 +0200

    Added some modifications to data-preload

--- server/data-preload.mjs
+++ server/data-preload.mjs
@@ -1,26 +1,40 @@
-import models from './models/index.mjs'
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
+import fs from "fs/promises";
 
-const preloadData = async () => {
-  await models.User.create({
-    email: 'andrei@nowhere.net',
-    passwordHash: await bcrypt.hash('welcome', 10),
-    type: 'regular'
-  })
-  await models.User.create({
-    email: 'andrei@somewhere.net',
-    passwordHash: await bcrypt.hash('welcome', 10),
-    type: 'regular'
-  })
-  await models.User.create({
-    email: 'andrei@admin.net',
-    passwordHash: await bcrypt.hash('welcome', 10),
-    type: 'admin'
-  })
-}
+const dbPath = "./db.sqlite";
 
-try {
-  await preloadData()
-} catch (error) {
-  console.warn(error)
-}
+const preloadData = async (models) => {
+  await models.User.create({
+    email: "andrei@nowhere.net",
+    passwordHash: await bcrypt.hash("welcome", 10),
+    type: "regular",
+  });
+  await models.User.create({
+    email: "andrei@somewhere.net",
+    passwordHash: await bcrypt.hash("welcome", 10),
+    type: "regular",
+  });
+  await models.User.create({
+    email: "andrei@admin.net",
+    passwordHash: await bcrypt.hash("welcome", 10),
+    type: "admin",
+  });
+};
+
+const deleteDatabaseAndPreload = async () => {
+  try {
+    console.log("Deleting database file...");
+    await fs.unlink(dbPath);
+    console.log("Database file deleted");
+
+    // Now import models AFTER deleting the database file
+    const models = (await import("./models/index.mjs")).default;
+
+    await preloadData(models);
+    console.log("Database preloaded successfully.");
+  } catch (error) {
+    console.error("Error deleting database:", error);
+  }
+};
+
+deleteDatabaseAndPreload();
